69ec4e226ea2416988e21456e827a90ef4d61e22
ll <ll>
2024-07-13 21:30:29 +0800

    Initial Commit

---
+++ WeavePlay/WeavePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeavePlayApp.swift
+//  WeavePlay
+//
+//  Created by 李龙 on 2024/7/13.
+//
+
+import SwiftUI
+
+@main
+struct WeavePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
